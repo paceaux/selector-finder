23bfb4eb89054b4250da1ef2c001b3e517e87774
paceaux <frank.m.taylor08@gmail.com>
2021-01-13 12:11:02 -0600

    autofixes lint complaints

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
@@ -6,88 +5,87 @@ const { LOG_FILE_NAME, SITEMAP_URL, DEFAULT_OUTPUT_FILE } = require('./src/const
 const SelectorFinder = require('./src/selector-finder');
 const Outputter = require('./src/outputter');
 const Log = require('./src/logger');
+
 const log = new Log(LOG_FILE_NAME);
 
-
-/* 
+/*
 CLI arguments
 
 node index.js --sitemap=https://wherever.com/xml --limit=20 --selector=".yourthing"
 node index.js -u https://wherever.com/xml -l 20 -s ".yourthing"
 */
-const argv = yargs(hideBin(process.argv))
-    .option('sitemap', {
-        alias: 'u',
-        description: 'url for the sitemap',
-        type: 'string',
-        default: SITEMAP_URL
-    })
-    .option('limit', {
-        alias: 'l',
-        description: 'how many pages to crawl',
-        type: 'number',
-        default: 0,
-    })
-    .option('selector', {
-        alias: 's',
-        description: 'css selector',
-        type: 'string',
-        default: '.title',
-    })
-    .option('takeScreenshots', {
-        alias: 'c',
-        description: 'Take a screenshot',
-        type: 'boolean',
-        default: false,
-    })
-    .option('isSpa', {
-        alias: 'd',
-        description: 'Is a Single Page Application',
-        type: 'boolean',
-        default: false,
-    })
-    .option('outputFileName', {
-        alias: 'o',
-        description: 'name of output file',
-        type: 'string',
-        default: DEFAULT_OUTPUT_FILE,
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+const { argv } = yargs(hideBin(process.argv))
+  .option('sitemap', {
+    alias: 'u',
+    description: 'url for the sitemap',
+    type: 'string',
+    default: SITEMAP_URL,
+  })
+  .option('limit', {
+    alias: 'l',
+    description: 'how many pages to crawl',
+    type: 'number',
+    default: 0,
+  })
+  .option('selector', {
+    alias: 's',
+    description: 'css selector',
+    type: 'string',
+    default: '.title',
+  })
+  .option('takeScreenshots', {
+    alias: 'c',
+    description: 'Take a screenshot',
+    type: 'boolean',
+    default: false,
+  })
+  .option('isSpa', {
+    alias: 'd',
+    description: 'Is a Single Page Application',
+    type: 'boolean',
+    default: false,
+  })
+  .option('outputFileName', {
+    alias: 'o',
+    description: 'name of output file',
+    type: 'string',
+    default: DEFAULT_OUTPUT_FILE,
+  })
+  .help()
+  .alias('help', 'h');
 
-const { sitemap, limit, selector, outputFileName, takeScreenshots, isSpa } = argv;
+const {
+  sitemap, limit, selector, outputFileName, takeScreenshots, isSpa,
+} = argv;
 
 async function main(sitemapUrl, limit, selector, outputFileName, takeScreenshots, isSpa) {
-    try {
-        const startMessage = 
-`
+  try {
+    const startMessage = `
 | Looking...                
 | Sitemap: ${sitemapUrl},   
 | limit: ${limit}           
 | CSS Selector: ${selector} 
-`
-        await log
-            .toConsole(startMessage)
-            .startTimer()
-            .infoToFileAsync();
-        const  result = await SelectorFinder.findSelectorAsync(sitemapUrl, limit, selector, takeScreenshots);
-        const { totalPagesSearched, pagesWithSelector, totalMatches } = result;
-        await Outputter.writeDataAsync(result, outputFileName);
+`;
+    await log
+      .toConsole(startMessage)
+      .startTimer()
+      .infoToFileAsync();
+    const result = await SelectorFinder.findSelectorAsync(sitemapUrl, limit, selector, takeScreenshots);
+    const { totalPagesSearched, pagesWithSelector, totalMatches } = result;
+    await Outputter.writeDataAsync(result, outputFileName);
 
-        log.endTimer();
-        const endMessage = `
+    log.endTimer();
+    const endMessage = `
 | Finished after ${log.elapsedTime}
 | Pages Scanned: ${totalPagesSearched} 
 | Pages with a Match: ${pagesWithSelector.length}
 | Total Results: ${totalMatches}                  
 | FileName: ${outputFileName}
 `;
-        await log.toConsole(endMessage, true).infoToFileAsync();
-    } catch (mainFunctionError) {
-        await log.errorToFileAsync(mainFunctionError);
-    }
+    await log.toConsole(endMessage, true).infoToFileAsync();
+  } catch (mainFunctionError) {
+    await log.errorToFileAsync(mainFunctionError);
+  }
 }
-
 
 main(sitemap, limit, selector, outputFileName, takeScreenshots, isSpa);
