67d2e7f5562aba0bbcc18e2185eece8e4ac2dab8
paceaux <frank.m.taylor08@gmail.com>
2021-01-19 10:04:55 -0600

    adds more constants and tests values

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,15 @@
 const LOG_FILE_NAME = 'log.txt';
 const SITEMAP_URL = 'https://www.exlrt.com/sitemap.xml';
 const DEFAULT_OUTPUT_FILE = 'pages.json';
+const IS_SPA = false;
+const LIMIT = 0;
+const TAKE_SCREENSHOTS = false;
 
 module.exports = {
   LOG_FILE_NAME,
   SITEMAP_URL,
   DEFAULT_OUTPUT_FILE,
+  IS_SPA,
+  LIMIT,
+  TAKE_SCREENSHOTS,
 };
---
+++ test/constants.test.js
@@ -0,0 +1,39 @@
+const {
+  LOG_FILE_NAME,
+  SITEMAP_URL,
+  DEFAULT_OUTPUT_FILE,
+  IS_SPA,
+  LIMIT,
+  TAKE_SCREENSHOTS,
+} = require('../src/constants');
+
+describe('constants', () => {
+  test('log file', () => {
+    expect(LOG_FILE_NAME).toBeTruthy();
+    expect(typeof LOG_FILE_NAME).toEqual('string');
+    expect(LOG_FILE_NAME).toEqual('log.txt');
+  });
+  test('sitemap url', () =>{
+    expect(SITEMAP_URL).toBeTruthy();
+    expect(typeof SITEMAP_URL).toEqual('string');
+  });
+  test('default file', () =>{
+    expect(DEFAULT_OUTPUT_FILE).toBeTruthy();
+    expect(typeof DEFAULT_OUTPUT_FILE).toEqual('string');
+    expect(DEFAULT_OUTPUT_FILE).toEqual('pages.json');
+  });
+  test('is spa', () =>{
+    expect(IS_SPA).toBeFalsy();
+    expect(typeof IS_SPA).toEqual('boolean');
+    expect(IS_SPA).toEqual(false);
+  });
+  test('limit', () =>{
+    expect(typeof LIMIT).toEqual('number');
+    expect(LIMIT).toEqual(0);
+  });
+  test('take screenshots', () =>{
+    expect(TAKE_SCREENSHOTS).toBeFalsy();
+    expect(typeof TAKE_SCREENSHOTS).toEqual('boolean');
+    expect(TAKE_SCREENSHOTS).toEqual(false);
+  });
+});
