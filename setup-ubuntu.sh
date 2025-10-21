727409cc3c196f0743ee198e6d1a81f76225d704
Paceaux <frank.m.taylor08@gmail.com>
2025-10-21 11:14:08 -0500

    tries different way to get around app armor

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -3,26 +3,4 @@
 # this is just for getting puppeteer to run in a github action
 # see https://pptr.dev/troubleshooting#issues-with-apparmor-on-ubuntu
 
-export CHROMIUM_BUILD_PATH=/@{HOME}/chromium/src/out/**/chrome
-cat | sudo tee /etc/apparmor.d/chrome-dev-builds <<EOF
-abi <abi/4.0>,
-include <tunables/global>
-
-profile chrome $CHROMIUM_BUILD_PATH flags=(unconfined) {
-  userns,
-
-  # Site-specific additions and overrides. See local/README for details.
-  include if exists <local/chrome>
-}
-EOF
-
-cd ~/.cache/puppeteer/chrome/linux-<version>/chrome-linux64/
-sudo chown root:root chrome_sandbox
-sudo chmod 4755 chrome_sandbox
-# copy sandbox executable to a shared location
-sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
-# export CHROME_DEVEL_SANDBOX env variable
-export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
-
-sudo service apparmor reload  # reload AppArmor profiles to include the new one
-
+echo 0 | sudo tee /proc/sys/kernel/apparmor_restrict_unprivileged_userns
