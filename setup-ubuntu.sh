6b1f8fca277f611df2ec46903ea449eb5ab0a126
Paceaux <frank.m.taylor08@gmail.com>
2025-10-21 11:11:56 -0500

    attempts to add sandbox

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -15,4 +15,14 @@ profile chrome $CHROMIUM_BUILD_PATH flags=(unconfined) {
   include if exists <local/chrome>
 }
 EOF
+
+cd ~/.cache/puppeteer/chrome/linux-<version>/chrome-linux64/
+sudo chown root:root chrome_sandbox
+sudo chmod 4755 chrome_sandbox
+# copy sandbox executable to a shared location
+sudo cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
+# export CHROME_DEVEL_SANDBOX env variable
+export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
+
 sudo service apparmor reload  # reload AppArmor profiles to include the new one
+
