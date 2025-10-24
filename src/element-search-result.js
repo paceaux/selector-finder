b3e400ef98f35f0676add2741f73c4e07082052b
Paceaux <frank.m.taylor08@gmail.com>
2025-10-23 19:25:27 -0500

    adds code docs for element-search-result

--- src/element-search-result.js
+++ src/element-search-result.js
@@ -1,6 +1,15 @@
 import * as cheerio from 'cheerio';
 
+/**
+ * A single search result that is an HTML element
+ * @class ElementSearchResult
+ * @classdesc A data structure for a single element that's a search result.
+ */
 export default class ElementSearchResult {
+  /**
+   * Creates an instance of ElementSearchResult
+   * @param {Object} element - The element object to extract information from
+   */
   constructor(element) {
     this.tag = element.name || element.localName;
     this.applyAttributes(element);
@@ -9,6 +18,13 @@ export default class ElementSearchResult {
     this.html = cheerio.load(element, null, false);
   }
 
+  /**
+   * Gets attributes from an element and returns them as an object
+   * @static
+   * @param {Object} element - The element to extract attributes from
+   * @returns {Object|null} An object containing attribute name-value pairs, or null if none
+   *  exist
+   */
   static extractAttributes(element) {
     let attributes = null;
     const elAttributes = element.attribs || element.attributes;
@@ -23,6 +39,10 @@ export default class ElementSearchResult {
     return attributes;
   }
 
+  /**
+   * Applies attributes from an element to this instance if they exist
+   * @param {Object} element - The element containing attributes to apply
+   */
   applyAttributes(element) {
     const attributes = element.attribs || element.attributes;
     if (attributes && Object.keys(attributes).length > 0) {
