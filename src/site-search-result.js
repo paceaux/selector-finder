d6c6166db5c17d3779ec7b300bfda7df37d04897
Paceaux <frank.m.taylor08@gmail.com>
2026-06-04 08:53:06 -0500

    adds JSDoc comments for siteSearchResult

--- src/site-search-result.js
+++ src/site-search-result.js
@@ -1,4 +1,11 @@
+/**
+ * @class SiteSearchResult
+ * @classdesc Represents search results for an entire site
+ */
 export default class SiteSearchResult extends Array {
+  /**
+ * @property {number} The total matches taken from all results
+ */
   get totalMatches() {
     let total = 0;
 
